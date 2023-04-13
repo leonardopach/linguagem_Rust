48a12cc960a4a6d8a80b6041b4e44062271395b7
leonardopach <pachecodossantos17@gmail.com>
2023-04-13 10:45:32 -0300

    rust

--- rust_1/src/main.rs
+++ rust_1/src/main.rs
@@ -1,41 +1,40 @@
-use yew::prelude::*;
+use fltk::{
+    app, button,
+    enums::{Color, Font, FrameType},
+    prelude::{GroupExt, WidgetExt},
+    window,
+};
 
-enum Msg {
-    AddOne,
-}
-
-struct CounterComponent {
-    count: i64,
-}
-
-impl Component for CounterComponent {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { count: 0 }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.count += 1;
-                true // re-render component
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let link = ctx.link();
-        html! {
-            <div class="container">
-                <p>{ self.count }</p>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-            </div>
-        }
-    }
-}
-
+use fltk_theme::{widget_themes, ThemeType, WidgetTheme};
 fn main() {
-    yew::start_app::<CounterComponent>();
+    let a = app::App::default();
+    let theme = WidgetTheme::new(ThemeType::Aero);
+    theme.apply();
+    let mut win = window::Window::default()
+        .with_size(400, 300)
+        .with_label("My window");
+
+    // win.set_color(Color::White);
+
+    let mut btn = button::Button::default()
+        .with_size(80, 80)
+        .center_of_parent()
+        .with_label("Click");
+
+    btn.set_frame(widget_themes::OS_DEFAULT_BUTTON_UP_BOX);
+    // theme_button(&mut btn);
+    win.end();
+    win.show();
+    btn.set_callback(move |b| {
+        b.set_label("Clicked");
+        win.set_label("clicked");
+    });
+    a.run().unwrap();
 }
+
+// fn theme_button(btn: &mut button::Button) {
+//     btn.clear_visible_focus();
+//     btn.set_color(Color::from_rgb(255, 0, 0).lighter());
+//     btn.set_selection_color(Color::from_rgb(255, 0, 0).darker());
+//     btn.set_frame(FrameType::RFlatBox)
+// }
